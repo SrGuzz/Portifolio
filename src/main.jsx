3544cad56503bcb040ef680d1de3c08482da3d11
Albert Luis Pereira de Jesus <albertluis123y88@gmail.com>
2025-09-01 09:13:55 -0300

    Correções

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import TaskPage from "./pages/TaskPage.jsx";
-// import ListaTarefas from "./pages/ListaTarefas.jsx";
-// import EnderecoPage from "./pages/EnderecoPage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  // {
-  //   path: "/ToDoList",
-  //   element: <ListaTarefas />,
-  // },
-  // {
-  //   path: "/task",
-  //   element: <TaskPage />,
-  // },
-  // {
-  //   path: "/Endereco",
-  //   element: <EnderecoPage />,
-  // },
-]);
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
